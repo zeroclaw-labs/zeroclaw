thanhdatio/TitoAI

fix(knowledge): address remaining CodeRabbit review issues
- MAJOR: Add db_path non-empty validation in Config::validate()
- MAJOR: Reject tags containing commas in add_node (comma is separator)
- MAJOR: Fix subgraph depth boundary (0..depth instead of 0..=depth)
- MAJOR: Apply project and node_type filters consistently in both
  tag-only and similarity search paths

src/tools/knowledge_tool.rs
@@ -184,9 +184,25 @@ impl KnowledgeTool {
             .and_then(|f| f.get("node_type"))
             .and_then(|v| v.as_str());
 
+        let filter_project = args
+            .get("filters")
+            .and_then(|f| f.get("project"))
+            .and_then(|v| v.as_str());
+
+        // Parse the node_type filter once so it applies in all code paths.
+        let parsed_filter_type = filter_type.and_then(|ft| NodeType::from_str(ft).ok());
+
         let results = if query.is_empty() && !filter_tags.is_empty() {
-            // Tag-only search.
-            let nodes = self.graph.query_by_tags(&filter_tags)?;
+            // Tag-only search -- apply node_type and project filters consistently.
+            let mut nodes = self.graph.query_by_tags(&filter_tags)?;
+            if let Some(ref nt) = parsed_filter_type {
+                nodes.retain(|n| &n.node_type == nt);
+            }
+            if let Some(proj) = filter_project {
+                nodes.retain(|n| {
+                    n.source_project.as_deref() == Some(proj)
+                });
+            }
             nodes
                 .into_iter()
                 .map(|node| json!({ "id": node.id, "type": node.node_type, "title": node.title, "score": 1.0 }))
@@ -195,10 +211,14 @@ impl KnowledgeTool {
             let mut search_results = self.graph.query_by_similarity(query, 20)?;
 
             // Post-filter by type if specified.
-            if let Some(ft) = filter_type {
-                if let Ok(nt) = NodeType::from_str(ft) {
-                    search_results.retain(|r| r.node.node_type == nt);
-                }
+            if let Some(ref nt) = parsed_filter_type {
+                search_results.retain(|r| &r.node.node_type == nt);
+            }
+            // Post-filter by project if specified.
+            if let Some(proj) = filter_project {
+                search_results.retain(|r| {
+                    r.node.source_project.as_deref() == Some(proj)
+                });
             }
             // Post-filter by tags if specified.
             if !filter_tags.is_empty() {

zeroclaw-labs/zeroclaw

src/tools/knowledge_tool.rs
@@ -181,9 +181,23 @@ impl KnowledgeTool {
             .and_then(|f| f.get("node_type"))
             .and_then(|v| v.as_str());
 
+        let filter_project = args
+            .get("filters")
+            .and_then(|f| f.get("project"))
+            .and_then(|v| v.as_str());
+
+        // Parse the node_type filter once so it applies in all code paths.
+        let parsed_filter_type = filter_type.and_then(|ft| NodeType::parse(ft).ok());
+
         let results = if query.is_empty() && !filter_tags.is_empty() {
-            // Tag-only search.
-            let nodes = self.graph.query_by_tags(&filter_tags)?;
+            // Tag-only search -- apply node_type and project filters consistently.
+            let mut nodes = self.graph.query_by_tags(&filter_tags)?;
+            if let Some(ref nt) = parsed_filter_type {
+                nodes.retain(|n| &n.node_type == nt);
+            }
+            if let Some(proj) = filter_project {
+                nodes.retain(|n| n.source_project.as_deref() == Some(proj));
+            }
             nodes
                 .into_iter()
                 .map(|node| json!({ "id": node.id, "type": node.node_type, "title": node.title, "score": 1.0 }))
@@ -192,10 +206,12 @@ impl KnowledgeTool {
             let mut search_results = self.graph.query_by_similarity(query, 20)?;
 
             // Post-filter by type if specified.
-            if let Some(ft) = filter_type {
-                if let Ok(nt) = NodeType::parse(ft) {
-                    search_results.retain(|r| r.node.node_type == nt);
-                }
+            if let Some(ref nt) = parsed_filter_type {
+                search_results.retain(|r| &r.node.node_type == nt);
+            }
+            // Post-filter by project if specified.
+            if let Some(proj) = filter_project {
+                search_results.retain(|r| r.node.source_project.as_deref() == Some(proj));
             }
             // Post-filter by tags if specified.
             if !filter_tags.is_empty() {